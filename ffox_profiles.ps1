cbacd96cdc15da89490224cbb781ee3a89cf6de5
equk <equk@users.noreply.github.com>
2018-09-19 18:27:19 +0100

    added main powershell script :shipit:

---
+++ ffox_profiles.ps1
@@ -0,0 +1,82 @@
+#requires -version 5.0
+#requires -runasadministrator
+
+<#
+.SYNOPSIS
+    Setup Multiple Firefox Profiles in Windows
+.DESCRIPTION
+    Setup Multiple Firefox Profiles in Windows
+    Create new firefox profiles with security & privacy settings already preset
+    Requires Admin Access
+    Twitter: @equilibriumuk
+.NOTES
+    File Name      : ffox_profiles.ps1
+    Author         : @equilibriumuk
+    Prerequisite   : PowerShell V5
+.LINK
+    Script posted on github:
+    https://github.com/equk
+#>
+
+$profiles = @(
+    "default"
+    "youtube"
+    "spotify"
+    "twitch"
+)
+
+$mydocs = [Environment]::GetFolderPath("MyDocuments")
+$profilepath = "$mydocs\ffox_profiles"
+$ffox_install = "C:\Program Files\Mozilla Firefox"
+$script_pwd = Get-Location
+$profile_template = ".\profile_template"
+
+if(-Not (Test-Path -Path $profilepath )){
+    Write-Host "!> Profile base folder not found"
+    Write-Host "+> Creating profile base folder $profilepath"
+    New-Item -ItemType directory -Path $profilepath
+}
+
+foreach ($profile in $profiles) {
+    if(-Not (Test-Path -Path $profilepath\$profile )){
+        Write-Host "!> Profile folder not found"
+        Write-Host "+> Creating profile folder $profilepath\$profile"
+        New-Item -ItemType directory -Path $profilepath\$profile
+    }
+    if(-Not (Test-Path -Path $profilepath\$profile\prefs.js )){
+        Write-Host "!> prefs.js not found"
+        Write-Host "+> Copying prefs.js for $profile"
+        cp $profile_template\prefs.js $profilepath\$profile\prefs.js
+    }
+    if(-Not (Test-Path -Path $profilepath\$profile\chrome\userChrome.css )){
+        Write-Host "!> userChrome.css not found"
+        Write-Host "+> Copying userChrome.css for $profile"
+        if(-Not (Test-Path -Path $profilepath\$profile\chrome )){
+            mkdir $profilepath\$profile\chrome
+        }
+        cp $profile_template\chrome\userChrome.css $profilepath\$profile\chrome\userChrome.css
+    }
+    if(-Not (Test-Path -Path "$ffox_install\firefox_$profile.exe" )){
+        Write-Host "!> No existing HardLink not found for $profile"
+        Write-Host "+> Creating HardLink from firefox.exe to firefox_$profile.exe"
+        cd $ffox_install
+        New-Item -ItemType HardLink -Name "firefox_$profile.exe" -Value "firefox.exe"
+        cd $script_pwd
+    }
+    if(-Not (Test-Path -Path "$profilepath\firefox_$profile.lnk" )){
+        Write-Host "!> No existing Shortcut not found for $profile"
+        Write-Host "+> Creating Shortcut to firefox_$profile.exe"
+        $WshShell = New-Object -comObject WScript.Shell
+        $Shortcut = $WshShell.CreateShortcut("$profilepath\firefox_$profile.lnk")
+        $Shortcut.TargetPath = "$ffox_install\firefox_$profile.exe"
+        $Shortcut.Arguments = "--profile $profilepath\$profile -no-remote"
+        $Shortcut.WorkingDirectory = "$ffox_install"
+        $Shortcut.Save()
+    }
+}
+Write-Host "----------------------------------------------------------"
+Write-Host " Shortcuts for each profile can be found in $profilepath"
+Write-Host " Opening $profilepath in explorer"
+Write-Host " You should now be able to pin each profile seperately"
+Write-Host "----------------------------------------------------------"
+Invoke-Item $profilepath
